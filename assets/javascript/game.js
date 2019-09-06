c66a0660c96a774ca9edd38df6202a84c48c1eef
Haakasaurus <kramerhjohnson@gmail.com>
2019-09-05 20:53:48 -0700

    Defined updateHiddenWord method.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ let musicalHangman = {
     possibleWordsHardMode: ['PLACEHOLDER'],
     secretWord: "",
     lettersGuessed: [],
+    hiddenWordArr: [],
     guessesLeft: 5,
     songsAndImages: {
         PLACEHOLDER: 'PLACEHOLDER'
@@ -20,7 +21,7 @@ let musicalHangman = {
     },
     /* This method takes in a secret word string and returns an array of 
         underscores representing the letters of the secret word.  If a 
-        character is not a letter then it is push into the array as is. */
+        character is not a letter then it is pushed into the array as is. */
     hideSecretWord: function (secretWord) {
         let hiddenWordArr = [];
         let nonAlphaChars = " !@#$%^&*()_-+={}[]|\\'\";:<>,./?";
@@ -38,6 +39,33 @@ let musicalHangman = {
         the hidden characters of the secret word. */
     displayHiddenWord: function (hiddenWordArr) {
         return hiddenWordArr.join(" ");
+    },
+    /* This method will be used to update the hidden word array to include
+        any new letters that have been guessed. */
+    updateHiddenWord: function (userGuess, secretWord, hiddenWordArr) {
+        let searchIndex = 0,
+            foundIndexes = [];
+        /* This loop will search the secret word for letters that match the
+            user's guess and return the index of the found element.  Looping
+            incrementing the searchIndex number will continue the search from 
+            the index of the last found element, continuing until no more 
+            matches are found. */
+
+        while (secretWord.indexOf(userGuess, searchIndex) != -1) {
+            foundIndexes.push(secretWord.indexOf(userGuess, searchIndex));
+            searchIndex += 1;
+        }
+        /* This will eliminate duplicate values that can result from the above
+            loop.  Creating a new Set from the foundIndexes automatically
+            eliminates duplicates because sets cannot have them.  Array.from() 
+            will create an array from the resulting set so that I can continue 
+            using arrays. */
+        foundIndexes = Array.from(new Set(foundIndexes));
+        /* This will loop through the indexes in foundIndexes and
+            replace the item at each index in hiddenWordArr with the user's guess. */
+        for (i = 0; i < foundIndexes.length; i++) {
+            hiddenWordArr = hiddenWordArr.splice(foundIndexes[i], 1, userGuess);
+        }
     },
     /* This method checks if a letter has already been guessed and returns true
        if it has, false otherwise. */
